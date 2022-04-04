6fb594a7068ba408fcf0b9765db55bec7255493f
uehara0528 <0528uehara@gmail.com>
2022-04-04 11:24:31 +0000

    [Finish]3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -10,12 +10,20 @@ class ListsController < ApplicationController
     redirect_to list_path(list.id)
   end
 
-   def show
+  def show
     @list = List.find(params[:id])
-   end
+  end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
 
  private
 
