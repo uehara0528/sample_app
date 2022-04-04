448299fa9a4e72731575fb1f53e608811a86e980
uehara0528 <0528uehara@gmail.com>
2022-04-04 07:03:26 +0000

    [Finish]3-4

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,23 +5,19 @@ class ListsController < ApplicationController
 
   def create
     list = List.new(list_params)
-
     list.save
 
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
 
-  def index
-    @lists = List.all
-  end
-
-  def show
-  end
+   def show
+    @list = List.find(params[:id])
+   end
 
   def edit
   end
 
-  private
+ private
 
   def list_params
     params.require(:list).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'top'=> 'homes#top'
   post 'lists'=>'lists#create'
   get 'lists'=> 'lists#index'
+
+  get 'lists/:id' => 'lists#shows',as: 'list'
 
 end
